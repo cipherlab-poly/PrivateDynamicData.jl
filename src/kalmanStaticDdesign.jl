a6fd4c091fbb1b4a91c5d59e04e14a43d82b9917
Jerome LE NY <jerome.le-ny@polymtl.ca>
2017-04-08 21:17:25 -0400

    First commit of differentially private Kalman filtering functions

--- src/kalmanStaticDdesign.jl
+++ src/kalmanStaticDdesign.jl
@@ -1,63 +1,45 @@
-# test.jl
-# Solves the matrix design problem for one agent in order to
-# enforce differential privacy in Kalman filtering
-
 using JuMP
 using Mosek
 
-# Problem data
-Ts = 1;
-A = [1 Ts;0 1];
-# A = [0 1;0 1];
+"""    computeDsteadyState(L::Matrix, A::Matrix, C::Matrix, V::Matrix,
+                           Winv::Matrix, α::Real, ρ::Real, Vinv=inv(V),
+                           m=size(A,1), p=size(C,1), r=size(L,1))
+Compute a static D matrix for signal preparation of one user, to be
+used for differentially private Kalman filtering.
+"""
+function computeDsteadyState(L, A, C, V, Winv, α, ρ, Vinv=inv(V),
+                             m=size(A,1), p=size(C,1), r=size(L,1))
+  modl = Model(solver=MosekSolver())
 
-n = size(A,1);  # state space dimension
+  @variable(modl, X[1:r, 1:r], SDP)
+  @variable(modl, Ω[1:m, 1:m], SDP)
+  @variable(modl, P[1:p, 1:p], SDP)
 
-# B = [Ts^2/2;Ts];
-# #sig = 1;
-#% #W = B*sig*B';
-# #W = eye(2);
-# W = 0.5*randn(2); W = W*W';
-W = 1e-1 * eye(n);
+  @objective(modl, Min, trace(X))
 
-C = eye(n);
-m = size(C,1);
+  @SDconstraint(modl, hvcat((2,2), X, L, L', Ω) >= zeros(r+m,r+m))
+  pibound = inv(α^2 * eye(p) + V);
+  @SDconstraint(modl, P <= pibound)
 
-Winv = inv(W);
-V = eye(m);
+  @SDconstraint(modl, hvcat((2,2),
+    C'*P*C-Ω+Winv, Winv*A, A'*Winv, Ω+A'*Winv*A) >= zeros(2m,2m))
 
-# Privacy multiplicative constant
-alpha = 1.7;
+  status = solve(modl)
 
-# Solve the SDP
-pibound = inv(alpha^2 * eye(m) + V);
+  #println("Solution status: ", status)
+  #println("Objective value: ", getobjectivevalue(modl))
+  P_val = getvalue(P)
+  #=
+  X_val = getvalue(X)
+  Ω_val = getvalue(Ω)
+  Σ_val = inv(Ω_val)
+  println("Difference between Σ^{-1} and (A Σ A^T + W)^{-1}+P")
+  E2 = inv(Σ_val)-inv(A*Σ_val*A'+W)-P_val
+  norm(E2)
+  =#
 
-mod = Model()
-@variable(mod, X[1:n, 1:n], SDP)
-@variable(mod, Ω[1:n, 1:n], SDP)
-@variable(mod, P[1:m, 1:m], SDP)
-
-@objective(mod, Min, trace(X))
-
-@SDconstraint(mod, hvcat((2,2), X, eye(n), eye(n), Ω) >= zeros(2n,2n))
-@SDconstraint(mod, P <= pibound)
-@SDconstraint(mod, hvcat((2,2),
-  C'*P*C-Ω+Winv, Winv*A, A'*Winv, Ω+A'*Winv*A) >= zeros(2n,2n))
-
-# Solver, recover and analyze the solution
-status = solve(mod)
-
-println("Solution status: ", status)
-println("Objective value: ", getobjectivevalue(mod))
-
-X_val = getvalue(X)
-Ω_val = getvalue(Ω)
-P_val = getvalue(P)
-
-Σ_val = inv(Ω_val)
-
-println("Difference between Σ^{-1} and (A Σ A^T + W)^{-1}+P")
-E2 = inv(Σ_val)-inv(A*Σ_val*A'+W)-P_val
-norm(E2)
-
-println("Matrix to factorize")
-M1 = alpha^2 * (inv(V-V*P_val*V)-inv(V))
+  # Matrix to factorize
+  M₁ = α^2 * (inv(V-V*P_val*V)-Vinv)
+  return chol(M₁)/ρ
+  #return D'/ρ
+end
