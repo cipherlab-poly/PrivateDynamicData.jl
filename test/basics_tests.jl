546534594152d763e3a4ad05247d576854b6911c
Jerome Le Ny <jerome.le-ny@polymtl.ca>
2022-09-04 14:54:32 -0400

    Actually doing the last update

--- test/basics_tests.jl
+++ test/basics_tests.jl
@@ -2,9 +2,9 @@
   @testset "Utils" begin
     ϵ = log(3); δ = 0.05
 
-    @test gaussianMechConstant(ϵ, δ) ≈ 1.7563398731147597 atol=1e-10
+    @test gaussianMechConstant(ϵ, δ) ≈ 1.7563398731147595 atol=1e-10
     tmp = gaussianMechConstant2(ϵ, δ)
-    @test tmp[1] ≈ 1.7563398731147597 atol=1e-10
+    @test tmp[1] ≈ 1.7563398731147595 atol=1e-10
     @test tmp[2] ≈ 2.3095249420840473 atol=1e-10
   end
 
@@ -13,20 +13,20 @@
 
     Random.seed!(12345)  # reseed the RNG
     D = Random.rand(100)
-    @test gaussianMech(D, Distributions.mean, 2.0/100, ϵ, δ)[1] == 0.4764940748202759
-    @test laplaceMech(D, Distributions.mean, 2.0/100, ϵ)[1] == 0.5366973705020675
-    @test mean_dp(D, ϵ, δ) == 0.47514367413387
-    @test mean_dp(D, ϵ, 0.0) == 0.4920613726730682
-    @test mean_dp(D, ϵ) == 0.4886977015049402
+    @test gaussianMech(D, Distributions.mean, 2.0/100, ϵ, δ)[1] == 0.4340762596257932
+    @test laplaceMech(D, Distributions.mean, 2.0/100, ϵ)[1] == 0.47917820856138704
+    @test mean_dp(D, ϵ, δ) == 0.4346391733818339
+    @test mean_dp(D, ϵ, 0.0) == 0.4721348509515663
+    @test mean_dp(D, ϵ) == 0.4769804801837357
 
     Random.seed!(12345)  # reseed the RNG
     f(x) = [x[1]^2*x[2], sqrt(x[1]), x[2]^3]
     x0 = [3, 4]
     l1sens = 0.5
     r,a = truncatedLaplaceMech(x0, f, l1sens, ϵ, δ)
-    @test r[1] == 36.090422106340114
-    @test r[2] == 2.50473361660456
-    @test r[3] == 63.800895516558114
+    @test r[1] == 37.503826058223474
+    @test r[2] == 1.4009933034124933 
+    @test r[3] == 63.277344064577306
     @test a == 2.020992083320228
   end
 end
